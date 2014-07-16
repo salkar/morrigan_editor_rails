500c060070133c7a4fbe4da61956bbf5fb4c922d
salkar <sokolov.sergey.a@gmail.com>
2014-07-16 13:37:01 +0400

    add separate precompilation for iframe.css assets + add code files

--- lib/morrigan_editor_rails/engine.rb
+++ lib/morrigan_editor_rails/engine.rb
@@ -1,5 +1,9 @@
 module MorriganEditorRails
   class Engine < ::Rails::Engine
     isolate_namespace MorriganEditorRails
+
+    initializer "morrigan_editor_rails.assets.precompile" do |app|
+        app.config.assets.precompile += %w(morrigan_editor_rails/iframe.css)
+      end
   end
 end
