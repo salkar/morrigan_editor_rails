19c10effec27284bd80f2c2eed591af8cd49ad63
salkar <sokolov.sergey.a@gmail.com>
2014-07-17 14:46:02 +0400

    add carrierwave image uploading by url

--- app/controllers/morrigan_editor_rails/editor_images_controller.rb
+++ app/controllers/morrigan_editor_rails/editor_images_controller.rb
@@ -4,7 +4,7 @@ module MorriganEditorRails
       if params["upload_img"]
         img_url = EditorImage.create(:image => params["upload_img"]).image.url
       else
-        img_url = params["upload_url"]       #TODO
+        img_url = EditorImage.create(:remote_image_url => params["upload_url"]).image.url
       end
       render :text => {:data => img_url}.to_json
     end
